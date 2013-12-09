5f3e215bcf39f0a7542b3bb8dfc458854ae25cde
raj <x>
2013-12-09 14:41:51 -0800

    Test Commit with Credential file

--- Python_Scripts/EC2/test_connection.py
+++ Python_Scripts/EC2/test_connection.py
@@ -1,7 +1,8 @@
 import boto.ec2
+
 #conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='AKIAIK7M6GAZAVLEBV3A',aws_secret_access_key='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')
 conn = boto.ec2.connect_to_region('us-west-2')
 
-resesrvations = conn.get_all_reservations()
+reservations = conn.get_all_reservations()
 
 print reservations
