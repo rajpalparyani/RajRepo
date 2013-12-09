d1867536ae5500e9b3df13e31750dfbb302d8dc6
raj <x>
2013-12-09 14:22:04 -0800

    Everything until first experiment

---
+++ Python_Scripts/EC2/test_connection.py
@@ -0,0 +1,7 @@
+import boto.ec2
+#conn = boto.ec2.connect_to_region("us-west-2",aws_access_key_id='AKIAIK7M6GAZAVLEBV3A',aws_secret_access_key='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')
+conn = boto.ec2.connect_to_region('us-west-2')
+
+resesrvations = conn.get_all_reservations()
+
+print reservations
